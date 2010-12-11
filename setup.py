1213d83fdcf82f7cca8196fada08e46b0e313ae6
Peter Ruibal <ruibalp@gmail.com>
2010-12-11 01:25:13 -0800

    Add remaining distutils metadata for setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from distutils.core import setup, Extension
 
 setup(name='python-libfov',
       version='0.1',
+      description='CPython Extension for libfov',
+      author='Peter Ruibal',
+      author_email='ruibalp@gmail.com',
+      url='https://github.com/fmoo/python-libfov',
       ext_modules = [
         Extension('fov', ['fov/fov.c'],
                   libraries=['fov'])
